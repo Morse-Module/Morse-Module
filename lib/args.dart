9965e36f0184f8149bade57fce6a78faee92e961
Matthew Gleich <matthewgleich@gmail.com>
2020-05-03 16:43:27 -0400

    Register the listStashes command

--- lib/args.dart
+++ lib/args.dart
@@ -14,6 +14,7 @@ void initParser(List<String> args) => CommandRunner(
   ..addCommand(DumpCommand())
   ..addCommand(InstallCommand())
   ..addCommand(RevertCommand())
+  ..addCommand(ListStashesCommand())
   ..run(args);
 
 /// Dump command

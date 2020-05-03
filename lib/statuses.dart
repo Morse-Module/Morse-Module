164638faa1d81c22b43a59c4763487003662231c
Matthew Gleich <matthewgleich@gmail.com>
2020-05-03 01:45:17 -0400

    Create step and success outputs

--- lib/statuses.dart
+++ lib/statuses.dart
@@ -4,10 +4,24 @@ import 'dart:io';
 // 📦 Package imports:
 import 'package:console/console.dart';
 
+/// Output for an error
 void error(String message) {
   final pen = TextPen();
   pen.red();
   pen('❌ $message');
   pen();
   exit(64); // Exit code for incorrect usage
+}
+
+/// Output for a step in the program
+void step(String message, String emoji) {
+  print('$emoji $message');
+}
+
+/// Output for a successful step
+void success(String message) {
+  final pen = TextPen();
+  pen.green();
+  pen('✅ $message');
+  pen();
 }
